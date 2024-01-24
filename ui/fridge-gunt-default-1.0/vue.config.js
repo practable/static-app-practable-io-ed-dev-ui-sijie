ec12574dca436cf6892f9ede4a041d549fccac41
Norazhous <SjieZHOU@outlook.com>
2024-01-24 17:07:59 +0000

    dist file upload

--- ui/fridge-gunt-default-1.0/vue.config.js
+++
@@ -1,9 +0,0 @@
-// vue.config.js
-module.exports = {
-    // options...
-    runtimeCompiler: true,
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/ui/fridge-gunt-default-1.0/'
-    : '/'
-  }
-  
